978c251931d373e35fddcd30531c2ec30af2b726
partycrusher546 <muthonidennis546@gmail.com>
2024-04-05 11:55:58 +0300

    complete task

---
+++ switch-swap/index.js
@@ -0,0 +1,14 @@
+function swapCase() {
+    const inputString = document.getElementById("inputString").value;
+    let swappedString = "";
+
+    for (const char of inputString) {
+        if (char === char.toUpperCase()) {
+            swappedString += char.toLowerCase();
+        } else {
+            swappedString += char.toUpperCase();
+        }
+    }
+
+    document.getElementById("output").textContent = swappedString;
+};
